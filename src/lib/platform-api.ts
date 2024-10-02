83f2310e25b1a947fa4bd099a68ac4d53ec9af19
TheEpicBlock <git.teb@theepicblock.nl>
2024-10-02 22:07:13 +0200

    Add proper typing to platform-api.ts

--- src/lib/platform-api.ts
+++ src/lib/platform-api.ts
@@ -1,34 +1,31 @@
 const API_URL = `http://localhost:7069`
 
-async function baseFetch(fetch: any, route: string): Promise<any> {
-    let result: any;
-    await fetch(`${API_URL}/${route}`)
-        .then((response: any) => response.json())
-        .then((data: any) => {
-            result = data;
-        })
-        .catch((error: any) => {
+type fetchT = typeof fetch;
+
+async function baseFetch(fetch: fetchT, route: string): Promise<any> {
+    return await fetch(`${API_URL}/${route}`)
+        .then((response) => response.json())
+        .catch((error) => {
             console.error(error)
         })
-    return result;
 }
 
-export async function fetchUsers(fetch: any): Promise<User[]> {
+export async function fetchUsers(fetch: fetchT): Promise<User[]> {
     return await baseFetch(fetch, `users`)
 }
 
-export async function fetchUser(fetch: any, id: string): Promise<User> {
+export async function fetchUser(fetch: fetchT, id: string): Promise<User> {
     return await baseFetch(fetch, `user/${id}`)
 }
 
-export async function fetchEvents(fetch: any): Promise<Event[]> {
+export async function fetchEvents(fetch: fetchT): Promise<Event[]> {
     return await baseFetch(fetch, `events`)
 }
 
-export async function fetchEvent(fetch: any, id: string): Promise<Event> {
+export async function fetchEvent(fetch: fetchT, id: string): Promise<Event> {
     return await baseFetch(fetch, `event/${id}`)
 }
 
-export async function fetchSubmissions(fetch: any, eventId: string): Promise<Submission[]> {
+export async function fetchSubmissions(fetch: fetchT, eventId: string): Promise<Submission[]> {
     return await baseFetch(fetch, `event/${eventId}/submissions`)
 }
