cf60f4d38790265da08c2d74128cb83735f4e2f1
sisby-folk <nora.herrah.heartstrings@gmail.com>
2025-02-22 10:02:25 +1100

    respect API_URL

--- lib/platform-api.tsx
+++ lib/platform-api.tsx
@@ -1,9 +1,10 @@
 import type { Submission, User } from "./types.d.tsx";
 
 type fetchT = typeof fetch;
+const API_URL = Deno.env.get("API_URL")
 
 async function baseFetch(fetch: fetchT, route: string): Promise<any> {
-  return await fetch(`${"https://platform.modfest.net"}/${route}`)
+  return await fetch(`${API_URL ?? "https://platform.modfest.net"}/${route}`)
     .then((response) => response.json())
     .catch((error) => {
       console.error(error);
