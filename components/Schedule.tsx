00a3b67f053ffaf369c3483394e1eec7a3e1c0bd
Szedann <65607498+Szedann@users.noreply.github.com>
2025-04-25 21:31:08 +0200

    fix table html

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -14,40 +14,42 @@ export function Schedule(
             <div key={entry.id} class="py-4">
               <h3 id={`event-${entry.id}`}>{entry.title}</h3>
               <table class="w-full">
-                <tr>
-                  <td>Type</td>
-                  <td>
-                    {entry.type?.split(" ").map((word) =>
-                      word.slice(0, 1).toUpperCase() + word.slice(1)
-                    ).join(" ") ?? "-"}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Time</td>
-                  <td>
-                    {entry.start
-                      ? <FormattedDateTime time={entry.start} />
-                      : "-"} - {entry.end
-                      ? <FormattedDateTime time={entry.end} timeOnly />
-                      : "-"}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Location</td>
-                  <td>
-                    {entry.location ?? "-"}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Hosts</td>
-                  <td>
-                    <Authors authors={entry.authors} users={props.users} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Description</td>
-                  <td class="w-full">{entry.description ?? "-"}</td>
-                </tr>
+                <tbody>
+                  <tr>
+                    <td>Type</td>
+                    <td>
+                      {entry.type?.split(" ").map((word) =>
+                        word.slice(0, 1).toUpperCase() + word.slice(1)
+                      ).join(" ") ?? "-"}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Time</td>
+                    <td>
+                      {entry.start
+                        ? <FormattedDateTime time={entry.start} />
+                        : "-"} - {entry.end
+                        ? <FormattedDateTime time={entry.end} timeOnly />
+                        : "-"}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Location</td>
+                    <td>
+                      {entry.location ?? "-"}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Hosts</td>
+                    <td>
+                      <Authors authors={entry.authors} users={props.users} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Description</td>
+                    <td class="w-full">{entry.description ?? "-"}</td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           );
