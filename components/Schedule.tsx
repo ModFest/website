f5b0f7cdc57011293e3c2b03ed70c0baee1c6fac
TheEpicBlock <git.teb@theepicblock.nl>
2025-04-25 18:59:55 +0200

    Put description on next line

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "preact/jsx-runtime";
 import FormattedDateTime from "../islands/DateTime.tsx";
 import { ScheduleEntryData, User } from "../lib/types.d.tsx";
 import UserLink from "./UserLink.tsx";
@@ -26,57 +27,58 @@ export function Schedule(props: {schedule: ScheduleEntryData[], users?: User[]})
               <th>
                 Hosts
               </th>
-              <th>
-                Summary
-              </th>
             </tr>
           </thead>
           <tbody>
             {props.schedule.map(entry => 
-              <tr key={entry.id}>
-                <td>
-                  {entry.title}
-                </td>
-                <td>
-                  {entry.type}
-                </td>
-                <td>
-                  {entry.location}
-                </td>
-                <td>
-                  {entry.start ? <FormattedDateTime time={entry.start}></FormattedDateTime> : "-"}
-                </td>
-                <td>
-                  {entry.end ? <FormattedDateTime time={entry.end} timeOnly></FormattedDateTime> : "-"}
-                </td>
-                <td>
-                  {
-                  // TODO split off into more generic stuff
-                    entry.authors.map(author => <UserLink key={author} id={author} users={props.users}></UserLink>)
-                    .map((userElement, index) => {
-                      let join = "";
-                      if (index === entry.authors.length - 1) {
-                        if (entry.authors.length > 2) {
-                          join = ", and ";
-                        } else if (entry.authors.length === 2) {
-                          join = " and ";
+              <Fragment key={entry.id}>
+                <tr>
+                  <td>
+                    {entry.title}
+                  </td>
+                  <td>
+                    {entry.type}
+                  </td>
+                  <td>
+                    {entry.location}
+                  </td>
+                  <td>
+                    {entry.start ? <FormattedDateTime time={entry.start}></FormattedDateTime> : "-"}
+                  </td>
+                  <td>
+                    {entry.end ? <FormattedDateTime time={entry.end} timeOnly></FormattedDateTime> : "-"}
+                  </td>
+                  <td>
+                    {
+                    // TODO split off into more generic stuff
+                      entry.authors.map(author => <UserLink key={author} id={author} users={props.users}></UserLink>)
+                      .map((userElement, index) => {
+                        let join = "";
+                        if (index === entry.authors.length - 1) {
+                          if (entry.authors.length > 2) {
+                            join = ", and ";
+                          } else if (entry.authors.length === 2) {
+                            join = " and ";
+                          }
+                        } else if (index > 0) {
+                          join = ", ";
                         }
-                      } else if (index > 0) {
-                        join = ", ";
-                      }
-                      return (
-                          <span key={index}>
-                        <span>{join}</span>
-                            {userElement}
-                      </span>
-                      );
-                    })
-                  }
-                </td>
-                <td>
-                  {entry.description}
-                </td>
-              </tr>
+                        return (
+                            <span key={index}>
+                          <span>{join}</span>
+                              {userElement}
+                        </span>
+                        );
+                      })
+                    }
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={6}>
+                    {entry.description}
+                  </td>
+                </tr>
+              </Fragment>
             )}
           </tbody>
         </table>
