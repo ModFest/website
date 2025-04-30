0b447741c476818e7c0bd1c0d94d6766a19466a2
Szedann <65607498+Szedann@users.noreply.github.com>
2025-04-30 14:26:17 +0200

    OG Tags

--- routes/[event].tsx
+++ routes/[event].tsx
@@ -4,6 +4,7 @@ import { fetchEvent, fetchEventSchedule, fetchEventSubmissions, fetchUser } from
 import { getPagesMarkdown } from "../lib/helpers.tsx";
 import { MarkdownBlocks } from "../components/MarkdownBlocks.tsx";
 import { Schedule } from "../components/Schedule.tsx";
+import { Head } from "$fresh/runtime.ts";
 
 export default async function Event(_req: Request, ctx: RouteContext) {
   const event = await fetchEvent(fetch, ctx.params.event);
@@ -23,82 +24,90 @@ export default async function Event(_req: Request, ctx: RouteContext) {
     
   const content = await getPagesMarkdown(`event/${ctx.params.event}`);
 
+  console.log(event.images)
+
   return (
-    <div
-      class="flex flex-col gap-4 mb-16"
-      style={`--color-mf-link: #${event.colors.primary}; --color-mf-event-background: #${event.colors.secondary}`}
-    >
+    <>
+      <Head>
+        <meta property="og:title" content={event.name} key="ogtitle"/>
+        <meta property="og:description" content={event.subtitle}/>
+      </Head>
       <div
-        class="card p-0 overflow-hidden relative aspect-[5/2]"
-        style={`background-color: #${event.colors.secondary}`}
+        class="flex flex-col gap-4 mb-16"
+        style={`--color-mf-link: #${event.colors.primary}; --color-mf-event-background: #${event.colors.secondary}`}
       >
-        <img
-          class="absolute w-full h-full inset-0 object-cover p-0"
-          src={event.images.background}
-          alt={`${event.name} cover image`}
-          aria-hidden="true"
-        />
-        <div class="absolute inset-0 z-[1] flex flex-col items-center justify-center p-8 gap-4">
+        <div
+          class="card p-0 overflow-hidden relative aspect-[5/2]"
+          style={`background-color: #${event.colors.secondary}`}
+        >
           <img
-            class="h-[50%]"
-            src={event.images.wordmark}
-            alt={`${event.name} wordmark`}
+            class="absolute w-full h-full inset-0 object-cover p-0"
+            src={event.images.background}
+            alt={`${event.name} cover image`}
             aria-hidden="true"
           />
-          {event.id.startsWith("ff")
-            ? (
-              <span class="font-medium text-2xl font-title uppercase text-mf-dark-translucent">
-                {event.subtitle}
-              </span>
-            )
-            : (
-              <span class="font-medium text-2xl font-title uppercase text-mf-light-translucent">
-                {event.subtitle}
-              </span>
+          <div class="absolute inset-0 z-[1] flex flex-col items-center justify-center p-8 gap-4">
+            <img
+              class="h-[50%]"
+              src={event.images.wordmark}
+              alt={`${event.name} wordmark`}
+              aria-hidden="true"
+            />
+            {event.id.startsWith("ff")
+              ? (
+                <span class="font-medium text-2xl font-title uppercase text-mf-dark-translucent">
+                  {event.subtitle}
+                </span>
+              )
+              : (
+                <span class="font-medium text-2xl font-title uppercase text-mf-light-translucent">
+                  {event.subtitle}
+                </span>
+              )}
+          </div>
+        </div>
+        <div class="justify-center flex gap-4">
+          {(event.phase === "planning" || event.phase === "modding") &&
+            (
+              <a
+                href="https://discord.gg/gn543Ee"
+                target="_blank"
+                class="button clickable"
+              >
+                Register on Discord!
+              </a>
+            )}
+          {event.phase === "showcase" && event.modpack &&
+            (
+              <a
+                href={event.modpack}
+                target="_blank"
+                class="button clickable"
+              >
+                Play!
+              </a>
+            )}
+          {event.phase === "complete" && event.modpack &&
+            (
+              <a
+                href={event.modpack}
+                target="_blank"
+                class="button clickable"
+              >
+                Download Modpack
+              </a>
+            )}
+          {submissions.length > 0 &&
+            (
+              <a href={`/${event.id}/submissions`} class="button clickable">
+                View Submissions
+              </a>
             )}
         </div>
-      </div>
-      <div class="justify-center flex gap-4">
-        {(event.phase === "planning" || event.phase === "modding") &&
-          (
-            <a
-              href="https://discord.gg/gn543Ee"
-              target="_blank"
-              class="button clickable"
-            >
-              Register on Discord!
-            </a>
-          )}
-        {event.phase === "showcase" && event.modpack &&
-          (
-            <a
-              href={event.modpack}
-              target="_blank"
-              class="button clickable"
-            >
-              Play!
-            </a>
-          )}
-        {event.phase === "complete" && event.modpack &&
-          (
-            <a
-              href={event.modpack}
-              target="_blank"
-              class="button clickable"
-            >
-              Download Modpack
-            </a>
-          )}
-        {submissions.length > 0 &&
-          (
-            <a href={`/${event.id}/submissions`} class="button clickable">
-              View Submissions
-            </a>
-          )}
-      </div>
       
-      <MarkdownBlocks content={content} />
-      {schedule && schedule.length > 0 ? <Schedule schedule={schedule} users={users}></Schedule> : null}
-    </div>
+        <MarkdownBlocks content={content} />
+        {schedule && schedule.length > 0 ? <Schedule schedule={schedule} users={users}></Schedule> : null}
+      </div>
+    </>
   );
 }
