5437c2a76fdae7c88044eecde07b264ee0ebedeb
Tomate0613 <69756782+tomate0613@users.noreply.github.com>
2025-06-24 12:06:46 +0200

    Put pages in an article tag

--- routes/pages/[page].tsx
+++ routes/pages/[page].tsx
@@ -4,5 +4,9 @@ import { MarkdownBlocks } from "../../components/MarkdownBlocks.tsx";
 
 export default async function Page(_req: Request, ctx: RouteContext) {
   const content = await getPagesMarkdown(ctx.params.page);
-  return <MarkdownBlocks content={content} />;
+  return (
+    <article>
+      <MarkdownBlocks content={content} />
+    </article>
+  );
 }
